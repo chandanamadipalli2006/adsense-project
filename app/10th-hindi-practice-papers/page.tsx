9de275ff0033721354ac5ab6cfbcbca62932823d
chandanamadipalli2006 <srimanichandanamadipalli2006@gmail.com>
2025-07-21 18:38:41 +0530

    Update page.tsx

--- app/10th-hindi-practice-papers/page.tsx
+++ app/10th-hindi-practice-papers/page.tsx
@@ -14,28 +14,28 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export default function TenthHindiModelPapers() {
   const modelPapers = [
-    {
-  title: "PRACTICE PAPER 2",
-  icon: FileSpreadsheet,
-  color: "bg-red-600 hover:bg-red-700",
-  description: "Government syllabus based practice paper",
-  link: "https://drive.google.com/file/d/1sY0OXHVIcIvz3AHaLuZLu4YSANYUb9Ql/view?usp=sharing",
-  downloadLink: "public/VC_PUBLICATIONS-10Th-FA1-Model_Paper-1.pdf",
-  downloadName: "VC_PUBLICATIONS-10Th-FA1-Model_Paper-1.pdf"
-},
-    {
-      name: "Greenera06.com Practice Papers",
-      items: [
-        {
-          title: "PRACTICE PAPER 1",
-          icon: FileSpreadsheet,
-          color: "bg-red-600 hover:bg-red-700",
-          description: "Government syllabus based practice paper",
-          link: "https://drive.google.com/file/d/19Ltal1MnQhynu9KGWRn9GaDB5VTC3YvB/view?usp=sharing",
-        },
-        
-      ],
-    },
+  {
+    name: "Greenera06.com Practice Papers",
+    items: [
+      {
+        title: "PRACTICE PAPER 2",
+        icon: FileSpreadsheet,
+        color: "bg-red-600 hover:bg-red-700",
+        description: "Government syllabus based practice paper",
+        link: "https://drive.google.com/file/d/1sY0OXHVIcIvz3AHaLuZLu4YSANYUb9Ql/view?usp=sharing",
+        downloadLink: "/VC_PUBLICATIONS-10Th-FA1-Model_Paper-1.pdf",
+        downloadName: "VC_PUBLICATIONS-10Th-FA1-Model_Paper-1.pdf"
+      },
+      {
+        title: "PRACTICE PAPER 1",
+        icon: FileSpreadsheet,
+        color: "bg-red-600 hover:bg-red-700",
+        description: "Government syllabus based practice paper",
+        link: "https://drive.google.com/file/d/19Ltal1MnQhynu9KGWRn9GaDB5VTC3YvB/view?usp=sharing",
+      },
+    ],
+  },
+
     {
       name: "Previous Year Papers",
       items: [
