8effd2b88d482bab4c94960fdbb5b72822184433
chandanamadipalli <22a81a0630@sves.org.in>
2026-02-06 10:35:22 +0530

    changes are  done

--- app/7th-hindi-model-papers/page.tsx
+++ app/7th-hindi-model-papers/page.tsx
@@ -82,8 +82,9 @@ export default function SeventhHindiModelPapers() {
           description: "Alternative annual exam pattern with marking scheme",
           link:
             "https://drive.google.com/file/d/1piwGuoDB2ry0YZVl0c1_GDhTww9_pb7O/view?usp=sharing",
-          download:
-            "/BALARAM-7-HIN-FA-4-MODEL-PAPER-2025-26.pdf",
+          
+           download: "/pdfs/BALARAM-7-HIN-FA-4-MODEL-PAPER-2025-26.pdf",
+
         },
         {
           title: "PRACTICE PAPER 1",
